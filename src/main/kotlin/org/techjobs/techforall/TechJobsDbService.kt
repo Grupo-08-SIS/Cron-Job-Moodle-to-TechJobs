39f19e145903e2a4a4fde6a5ee04256846445cac
RafaelH31 <rafael.cebrian@sptech.school>
2024-11-05 01:14:57 -0300

    fix: duplicação da pontuação

--- src/main/kotlin/org/techjobs/techforall/TechJobsDbService.kt
+++ src/main/kotlin/org/techjobs/techforall/TechJobsDbService.kt
@@ -2,6 +2,7 @@ package org.techjobs.techforall
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 import org.techjobs.techforall.config.TechJobsDbConfig
@@ -18,12 +19,15 @@ class TechJobsDbService(
     fun cadastrarCursos(cursos: List<CursoMoodleDto>) {
 
         cursos.forEach { curso ->
-
-            val categoriaExistente = jdbcTemplateTechJobs.queryForObject(
-                "SELECT id FROM categoria WHERE nome = ?",
-                arrayOf(curso.categoria),
-                Long::class.java
-            )
+            val categoriaExistente = try {
+                jdbcTemplateTechJobs.queryForObject(
+                    "SELECT id FROM categoria WHERE nome = ?",
+                    arrayOf(curso.categoria),
+                    Long::class.java
+                )
+            } catch (e: EmptyResultDataAccessException) {
+                null
+            }
 
             val categoriaId = categoriaExistente ?: run {
                 val sqlInserirCategoria = "INSERT INTO categoria (nome) VALUES (?)"
@@ -45,7 +49,6 @@ class TechJobsDbService(
                 curso.id, curso.nome
             )
 
-            // Inserir a relação ManyToMany na tabela curso_categoria
             val sqlInserirRelacao = """
             INSERT INTO curso_categoria (curso_id, categoria_id)
             VALUES (?, ?)
@@ -55,43 +58,88 @@ class TechJobsDbService(
                 curso.id, categoriaId
             )
 
-//            val sqlInserirCurso = """
-//            INSERT INTO curso (id, nome, categoria, total_atividades, total_atividades_do_aluno)
-//            VALUES (?, ?, ?, ?, ?)
-//            ON DUPLICATE KEY UPDATE
-//            nome = VALUES(nome),
-//            categoria = VALUES(categoria),
-//            total_atividades = VALUES(total_atividades),
-//            total_atividades_do_aluno = VALUES(total_atividades_do_aluno)
-//        """
-//            jdbcTemplateTechJobs.update(
-//                sqlInserirCurso,
-//                curso.id, curso.nome, curso.categoria, curso.totalAtividades, curso.totalAtividadesDoAluno
-//            )
+            val sqlInserirCursoPontos = """
+            INSERT INTO curso (id, nome, total_atividades, total_atividades_do_aluno)
+            VALUES (?, ?, ?, ?)
+            ON DUPLICATE KEY UPDATE
+            nome = VALUES(nome),
+            total_atividades = VALUES(total_atividades),
+            total_atividades_do_aluno = VALUES(total_atividades_do_aluno)
+        """
+            jdbcTemplateTechJobs.update(
+                sqlInserirCursoPontos,
+                curso.id, curso.nome, curso.totalAtividades, curso.totalAtividadesDoAluno
+            )
         }
     }
 
     fun cadastrarPontuacoes(pontuacoes: List<PontuacaoMoodleDto>) {
-
         pontuacoes.forEach { pontuacao ->
-            val sqlInserirPontuacao = """
-            INSERT INTO pontuacao (aluno_id, aluno_email, curso_id, curso_nome, data_entrega, nome_atividade, nota_atividade, nota_aluno)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            ON DUPLICATE KEY UPDATE
-            data_entrega = VALUES(data_entrega),
-            nota_aluno = VALUES(nota_aluno)
-        """
-            jdbcTemplateTechJobs.update(
-                sqlInserirPontuacao,
-                pontuacao.alunoId,
-                pontuacao.alunoEmail,
-                pontuacao.cursoId,
-                pontuacao.cursoNome,
-                pontuacao.dataEntrega,
-                pontuacao.nomeAtividade,
-                pontuacao.notaAtividade,
-                pontuacao.notaAluno
-            )
+
+            val cursoExistente = try {
+                jdbcTemplateTechJobs.queryForObject(
+                    "SELECT id FROM curso WHERE id = ?",
+                    arrayOf(pontuacao.cursoId),
+                    Long::class.java
+                )
+            } catch (e: EmptyResultDataAccessException) {
+                null
+            }
+
+            // If curso_id does not exist, insert it
+            if (cursoExistente == null) {
+                val sqlInserirCurso = """
+                INSERT INTO curso (id, nome, total_atividades, total_atividades_do_aluno)
+                VALUES (?, ?, ?, ?)
+                ON DUPLICATE KEY UPDATE
+                nome = VALUES(nome),
+                total_atividades = VALUES(total_atividades),
+                total_atividades_do_aluno = VALUES(total_atividades_do_aluno)
+            """
+                jdbcTemplateTechJobs.update(
+                    sqlInserirCurso,
+                    pontuacao.cursoId, pontuacao.cursoNome, 0, 0
+                )
+            }
+
+            val alunoExistente = try {
+                jdbcTemplateTechJobs.queryForObject(
+                    "SELECT id FROM aluno WHERE id = ?",
+                    arrayOf(pontuacao.alunoId),
+                    Long::class.java
+                )
+            } catch (e: EmptyResultDataAccessException) {
+                null
+            }
+
+
+            val pontuacaoExistente = try {
+                jdbcTemplateTechJobs.queryForObject(
+                    "SELECT 1 FROM pontuacao WHERE aluno_id = ? AND curso_id = ? AND nome_atividade = ?",
+                    arrayOf(pontuacao.alunoId, pontuacao.cursoId, pontuacao.nomeAtividade),
+                    Int::class.java
+                )
+            } catch (e: EmptyResultDataAccessException) {
+                null
+            }
+
+            if (pontuacaoExistente == null) {
+                val sqlInserirPontuacao = """
+                INSERT INTO pontuacao (aluno_id, aluno_email, curso_id, curso_nome, data_entrega, nome_atividade, nota_atividade, nota_aluno)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            """
+                jdbcTemplateTechJobs.update(
+                    sqlInserirPontuacao,
+                    pontuacao.alunoId,
+                    pontuacao.alunoEmail,
+                    pontuacao.cursoId,
+                    pontuacao.cursoNome,
+                    pontuacao.dataEntrega,
+                    pontuacao.nomeAtividade,
+                    pontuacao.notaAtividade,
+                    pontuacao.notaAluno
+                )
+            }
         }
     }
 
